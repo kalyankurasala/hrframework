91ed094084bc40a70066b862be1404ea16a81731
K Avinash <avinash@optionm.com>
2018-08-09 18:49:55 +0530

    add user modal in manage jobs

--- src/app/admin/recruitment/manage-jobs/manage-jobs.component.ts
+++ src/app/admin/recruitment/manage-jobs/manage-jobs.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModalDirective } from 'ngx-bootstrap';
+import { FormGroup, FormBuilder,  FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-manage-jobs',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-jobs.component.css']
 })
 export class ManageJobsComponent implements OnInit {
+  
+  
+  userEditForm: FormGroup;
 
-  constructor() { }
 
+  constructor(private fb: FormBuilder) {
+    this.createEditForm();
+  }
   ngOnInit() {
   }
+  createEditForm() {
+    this.userEditForm = this.fb.group({
+      FirstName: new FormControl,
+      Lastname: new FormControl,
+      EmailID: new FormControl
+    });
+  }
+  @ViewChild('addUserModal') public addUserModal: ModalDirective;
 
+  AddUserModalfn(e) {
+    if (e != 0) {
+      this.addUserModal.show();
+    } else {
+      this.addUserModal.hide();
+    }
+  }
 }
