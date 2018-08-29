56409c1788117a356c8eeb6536c8d1bb2604a517
Kalyan Swamy <Swamy@optionmatrix.com>
2018-08-29 11:38:04 +0530

    dummy

--- src/app/admin/recruitment/recruitment-ats/ats-timeline/ats-timeline.component.spec.ts
+++ src/app/admin/recruitment/recruitment-ats/ats-timeline/ats-timeline.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AtsTimelineComponent } from './ats-timeline.component';
 
 describe('AtsTimelineComponent', () => {
