70b17c28611001d597d4f406685579facb461f25
kalyankurasala <kalyank94@gmail.com>
2018-09-11 15:23:12 +0530

    alignment design

--- src/app/admin/associate/alignment/alignment.component.ts
+++ src/app/admin/associate/alignment/alignment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'angular-highcharts';
 
 @Component({
   selector: 'app-alignment',
@@ -12,4 +13,33 @@ export class AlignmentComponent implements OnInit {
   ngOnInit() {
   }
 
+  quartileChart = new Chart({
+    chart: {
+        type: 'line'
+    },
+    title: {
+        text: 'Quartile Ranking'
+    },
+    subtitle: {
+        text: ''
+    },
+    xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    },
+    yAxis: {
+        categories: ['Q0', 'Q4', 'Q3', 'Q2', 'Q1']
+    },
+    plotOptions: {
+        line: {
+            dataLabels: {
+                enabled: true
+            },
+            enableMouseTracking: false
+        }
+    },
+    series: [{
+        name: 'Quartile Ranking',
+        data: [1, 2, 3, 4, 3, 2, 1, 2, 1, 3, 4, 1]
+    } ]
+});
 }
